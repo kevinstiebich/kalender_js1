7ca1753c2f5c59c2fe8e5cfd563abc59f7a802fc
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-29 16:56:44 +0200

    functions für doomsday und Startwochentag erstellen

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ const weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Fr
 const numberOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const numberOfWeekdays = ["erste", "zweite", "dritte", "vierte", "fünfte"];
 
+//macht den Introtext dynamisch, immer auf den heutigen Tag bezogen
+let intro = document.getElementById("intro");
+intro.textContent = "Der " + String(date.getDate()).padStart(2, "0") + ". " + months[date.getMonth()] + " " + date.getFullYear() + " ist ein " + weekdays[date.getDay()] + " und zwar der " + 
+getNumberOfWeekdays() + " " + weekdays[date.getDay()] + " im Monat " + months[date.getMonth()] + " des Jahres " + date.getFullYear() + ". Es handelt sich um den " + 
+dayOfTheYear() + ". Tag des Jahres, was bedeutet, dass es noch " + daysRemaining() + " Tage bis zum Jahresende sind. Der Monat " + months[date.getMonth()] + " hat insgesamt " + numberOfDays[date.getMonth()] + 
+" Tage. Heute ist " + isHoliday() + " gesetzlicher Feiertag in Deutschland."
+
 //rechnet aus der wievielte Tag des Jahres heute ist
 function dayOfTheYear() {
     let days = date.getDate();
@@ -47,9 +54,129 @@ function getNumberOfWeekdays() {
     } else return numberOfWeekdays[Math.floor((date.getDate() / 7))];
 }
 
-//macht den Introtext dynamisch, immer auf den heutigen Tag bezogen
-let intro = document.getElementById("intro");
-intro.textContent = "Der " + String(date.getDate()).padStart(2, "0") + ". " + months[date.getMonth()] + " " + date.getFullYear() + " ist ein " + weekdays[date.getDay()] + " und zwar der " + 
-getNumberOfWeekdays() + " " + weekdays[date.getDay()] + " im Monat " + months[date.getMonth()] + " des Jahres " + date.getFullYear() + ". Es handelt sich um den " + 
-dayOfTheYear() + ". Tag des Jahres, was bedeutet, dass es noch " + daysRemaining() + " Tage bis zum Jahresende sind. Der Monat " + months[date.getMonth()] + " hat insgesamt " + numberOfDays[date.getMonth()] + 
-" Tage. Heute ist " + isHoliday() + " gesetzlicher Feiertag in Deutschland."
+//____________________________@REVIEWER: Hier kannst du erstmal aufhören zu lesen_____________________________________________________________________________________________
+
+//rechnet aus, welcher Tag dieses Jahr der Doomsday ist (funktioniert nur für die Jahre 2000-2099)
+function getDoomsday() {
+    let quotient = Math.floor(date.getFullYear() - 2000) / 12;
+    let rest = (date.getFullYear() - 2000) % 12;
+    let restDurch4 = Math.floor(rest / 4);
+    let addiert = quotient + rest + restDurch4;
+    let plusJahrhundertAnker = addiert + 2; //für 2000er Jahre
+    let doomsdayNr = Math.floor(plusJahrhundertAnker % 7);
+    return doomsdayNr;
+}
+
+//rechnet aus, auf welchen Wochentag der 1. dieses Monats fällt
+function getFirstWeekday() {
+    let firstWeekdayNr;
+    let doomsday = getDoomsday();
+    let moduloThisMonth;
+
+    switch (date.getMonth()) {
+        case 0:
+            if ((date.getFullYear() % 4 == 0 && date.getFullYear() % 100 != 0) || date.getFullYear() % 400 == 0) {
+                moduloThisMonth = (4 - 1) % 7
+            } else moduloThisMonth = (3 - 1) % 7;
+
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 1:
+            if ((date.getFullYear() % 4 == 0 && date.getFullYear() % 100 != 0) || date.getFullYear() % 400 == 0) {
+                moduloThisMonth = (29 - 1) % 7
+            } else moduloThisMonth = (28 - 1) % 7;
+
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 2:
+            moduloThisMonth = (14 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 3:
+            moduloThisMonth = (4 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 4:
+            moduloThisMonth = (9 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 5:
+            moduloThisMonth = (6 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 6:
+            moduloThisMonth = (11 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 7:
+            moduloThisMonth = (8 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 8:
+            moduloThisMonth = (5 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 9:
+            moduloThisMonth = (10 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 10:
+            moduloThisMonth = (7 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        case 11:
+            moduloThisMonth = (12 - 1) % 7;
+            if (doomsday >= moduloThisMonth) {
+                firstWeekdayNr = doomsday - moduloThisMonth;
+            } else {
+                firstWeekdayNr = doomsday + 7 - moduloThisMonth;
+            }
+            break;
+        }
+
+    return firstWeekdayNr;
+}
